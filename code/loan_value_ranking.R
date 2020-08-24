7213de70bfd857efb90d5a11fdda72c43929592f
John McCambridge <68754979+JohnMcCambridge@users.noreply.github.com>
2020-08-24 17:40:25 +0100

    Updated to load in 0808 data release
    Updated to load in 0808 data release, also added code calculating revenues earned by banks

--- code/loan_value_ranking.R
+++ code/loan_value_ranking.R
@@ -9,29 +9,29 @@
 # Set numeric min, mid and max values for all loan ranges, keep exact values as-is
 adbs <- adbs %>% 
   mutate(LoanRangeMin = case_when(!is.na(LoanAmount) ~ as.numeric(LoanAmount),
-                                  is.na(LoanAmount) & adbs$LoanRange == "a $5-10 million"       ~ as.numeric( 5000000),
-                                  is.na(LoanAmount) & adbs$LoanRange == "b $2-5 million"        ~ as.numeric( 2000000),
-                                  is.na(LoanAmount) & adbs$LoanRange == "c $1-2 million"        ~ as.numeric( 1000000),
-                                  is.na(LoanAmount) & adbs$LoanRange == "d $350,000-1 million"  ~ as.numeric(  350000),
-                                  is.na(LoanAmount) & adbs$LoanRange == "e $150,000-350,000"    ~ as.numeric(  150000),
+                                  is.na(LoanAmount) & LoanRange == "a $5-10 million"       ~ as.numeric( 5000000),
+                                  is.na(LoanAmount) & LoanRange == "b $2-5 million"        ~ as.numeric( 2000000),
+                                  is.na(LoanAmount) & LoanRange == "c $1-2 million"        ~ as.numeric( 1000000),
+                                  is.na(LoanAmount) & LoanRange == "d $350,000-1 million"  ~ as.numeric(  350000),
+                                  is.na(LoanAmount) & LoanRange == "e $150,000-350,000"    ~ as.numeric(  150000),
                                   TRUE ~ NA_real_))
 
 adbs <- adbs %>% 
   mutate(LoanRangeMax = case_when(!is.na(LoanAmount) ~ as.numeric(LoanAmount),
-                                  is.na(LoanAmount) & adbs$LoanRange == "a $5-10 million"       ~ as.numeric(10000000),
-                                  is.na(LoanAmount) & adbs$LoanRange == "b $2-5 million"        ~ as.numeric( 5000000),
-                                  is.na(LoanAmount) & adbs$LoanRange == "c $1-2 million"        ~ as.numeric( 2000000),
-                                  is.na(LoanAmount) & adbs$LoanRange == "d $350,000-1 million"  ~ as.numeric( 1000000),
-                                  is.na(LoanAmount) & adbs$LoanRange == "e $150,000-350,000"    ~ as.numeric(  350000),
+                                  is.na(LoanAmount) & LoanRange == "a $5-10 million"       ~ as.numeric(10000000),
+                                  is.na(LoanAmount) & LoanRange == "b $2-5 million"        ~ as.numeric( 5000000),
+                                  is.na(LoanAmount) & LoanRange == "c $1-2 million"        ~ as.numeric( 2000000),
+                                  is.na(LoanAmount) & LoanRange == "d $350,000-1 million"  ~ as.numeric( 1000000),
+                                  is.na(LoanAmount) & LoanRange == "e $150,000-350,000"    ~ as.numeric(  350000),
                                   TRUE ~ NA_real_))
 
 adbs <- adbs %>% 
   mutate(LoanRangeMid = case_when(!is.na(LoanAmount) ~ as.numeric(LoanAmount),
-                                  is.na(LoanAmount) & adbs$LoanRange == "a $5-10 million"       ~ as.numeric( 7500000),
-                                  is.na(LoanAmount) & adbs$LoanRange == "b $2-5 million"        ~ as.numeric( 3500000),
-                                  is.na(LoanAmount) & adbs$LoanRange == "c $1-2 million"        ~ as.numeric( 1500000),
-                                  is.na(LoanAmount) & adbs$LoanRange == "d $350,000-1 million"  ~ as.numeric(  675000),
-                                  is.na(LoanAmount) & adbs$LoanRange == "e $150,000-350,000"    ~ as.numeric(  250000),
+                                  is.na(LoanAmount) & LoanRange == "a $5-10 million"       ~ as.numeric( 7500000),
+                                  is.na(LoanAmount) & LoanRange == "b $2-5 million"        ~ as.numeric( 3500000),
+                                  is.na(LoanAmount) & LoanRange == "c $1-2 million"        ~ as.numeric( 1500000),
+                                  is.na(LoanAmount) & LoanRange == "d $350,000-1 million"  ~ as.numeric(  675000),
+                                  is.na(LoanAmount) & LoanRange == "e $150,000-350,000"    ~ as.numeric(  250000),
                                   TRUE ~ NA_real_))
 
 # rank all (we can then subset this at any grain and still get accurate rank order results)
@@ -79,3 +79,38 @@ toptail_avgloan_zips <- rbind(a,b[order(b$LoanValueRank),])
 toptail_avgloan_zips <- arrange(toptail_avgloan_zips, State)
 # slice out one specific state using filter, and create nicer looking table using knitr
 knitr::kable(filter(toptail_avgloan_zips, State == "FL"), digits = 0, format.args = list(big.mark = ",", scientific = FALSE))
+
+
+
+### Lender and Agent Processing Fees ---------------------------------------
+
+# based on https://home.treasury.gov/system/files/136/PPP%20Lender%20Information%20Fact%20Sheet.pdf
+#
+# SBA pays lender:
+#  Five (5) percent for loans of not more than $350,000;
+#  Three (3) percent for loans of more than $350,000 and less than $2,000,000; and
+#  One (1) percent for loans of at least $2,000,000. 
+#
+# Lender pays agent (if applicable):
+#  One (1) percent for loans of not more than $350,000;
+#  0.50 percent for loans of more than $350,000 and less than $2 million; and
+#  0.25 percent for loans of at least $2 million
+
+adbs <- adbs %>% 
+  mutate(LenderRevenueMin = case_when((LoanRangeMin >= 2000000)                              ~ (0.01*LoanRangeMin),
+                                      (LoanRangeMin <  2000000 & adbs$LoanRangeMin > 350000) ~ (0.03*LoanRangeMin),
+                                      (LoanRangeMin <=  350000)                              ~ (0.05*LoanRangeMin),
+                                      TRUE ~ NA_real_))
+
+lenderrevenue <- adbs %>% 
+  group_by(Lender) %>%
+  summarise(SumLoanRangeMin = sum(LoanRangeMin),
+            LoanCount = n(),
+            AvgLoanMin = SumLoanRangeMin/LoanCount,
+            SumLenderRevenueMin = sum(LenderRevenueMin),
+            AvgLenderRevenueMinLoan = SumLenderRevenueMin/LoanCount
+  ) %>%
+  mutate(SumLenderRevenueMinRank = rank(-SumLenderRevenueMin, ties.method = "min"),
+         AvgLenderRevenueMinLoanRank = rank(-AvgLenderRevenueMinLoan, ties.method = "min")) %>% 
+  arrange(SumLenderRevenueMinRank)
+
