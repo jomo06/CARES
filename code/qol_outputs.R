ca24a872a93f51a63b76e8befc18dfe6c89ada31
John McCambridge <68754979+JohnMcCambridge@users.noreply.github.com>
2020-09-06 22:19:51 +0100

    Added output codes for various CSVs for journalists
    added one enhanced CSV per state
    added a CSV showing only loans that have a shared address with other loans, used then for a XLS pivot table

--- code/qol_outputs.R
+++ code/qol_outputs.R
@@ -1,3 +1,20 @@
 ### to be run after ppp_data_marge_0808.R
 ### john@mccambridge.co
 
+### create outputs of one enhanced CSV per state -------------------------------
+
+# by(adbs, adbs$State, FUN=function(i) write.csv(i, paste0("PPP Loans 0808 Enhanced ",i$State[1], ".csv")))
+
+adbs %>%
+    group_by(State) %>%
+    group_walk(~ write_csv(.x, file.path("data/adbs_bystate", paste0("PPP Loans 0808 Enhanced ", .y$State, ".csv"))))
+  
+  
+### create outputs per state of businesses with shared addresses ---------------
+
+sharedaddress_bystate <- adbs %>% filter(!is.na(Address)) %>%
+  group_by(State, Address) %>%
+  summarise(SumLoanRangeMin = sum(LoanRangeMin),
+            LoanCount = n()
+            ) %>% filter(LoanCount > 1)
+
